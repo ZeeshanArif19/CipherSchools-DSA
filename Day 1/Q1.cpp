cab2278f1c9489a80d7ed58558a82e477a606398
ZeeshanArif19 <149928037+ZeeshanArif19@users.noreply.github.com>
2024-08-25 13:00:07 +0530

    Add files via upload

---
+++ Day 1/Q1.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string a,b;
+    cin>>a>>b;
+    a.append(b);
+    cout<<"Concatenated String: "<<a;
+}
---
+++ Day 1/Q2.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a,b,c,d,e;
+    cin>>a>>b>>c>>d>>e;
+    float average= (a+b+c+d+e)/5.0;
+    cout<<"The average grade is: "<<average;
+}
