3629d755a284adbfc55f895fdae8d568d28bc387
ZeeshanArif19 <149928037+ZeeshanArif19@users.noreply.github.com>
2024-08-25 13:45:36 +0530

    Add files via upload

---
+++ Day 3/Q2.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+
+    int factorial=1;
+    for(int i=1;i<=n;i++) factorial*=i;
+    cout<<"The factorial of "<<n<<" is: "<<factorial;
+}
